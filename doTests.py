591f5c3d4e74deff86f5429bb3050369a528ebdf
akhil-rana <akhilrana0001@gmail.com>
2022-12-07 20:54:31 +0530

    Update: code restructured

--- doTests.py
+++ doTests.py
@@ -1,26 +1,29 @@
 import subprocess
 import time
 
-digitSizes = [100, 1000, 10000, 100000, 1000000, 10000000]
+# digitSizes = [100, 1000, 10000, 100000, 500000, 1000000, 5000000, 10000000]
+digitSizes = [100, 1000, 10000, 100000, 500000]
 
 
 # generate random digits and write to file
 def generateRandomDigitsFiles(size):
-    subprocess.call(['python', 'randomDigitGen.py', 'input.txt', str(size)])
+    subprocess.call(['python', 'randomDigitGen.py',
+                    './src/input.txt', str(size)])
     print('\nGenerated random digits of size ' + str(size))
 
 
 def runCompress(size):  # run compress.py from command line and find the execution time
     start = time.time()
-    subprocess.call(['python', 'compress.py', 'input.txt', 'output.compress'])
+    subprocess.call(['python', './src/compress.py',
+                    './src/input.txt', './src/compressed.bin'])
     end = time.time()
     print('For {} digits compression time: {} seconds'.format(size, end - start))
 
 
 def runDecompress(size):  # run decompress.py from command line and find the execution time
     start = time.time()
-    subprocess.call(['python', 'decompress.py',
-                    'output.compress', 'output.txt'])
+    subprocess.call(['python', './src/decompress.py',
+                    './src/compressed.bin', './src/output.txt'])
     end = time.time()
     print('For {} digits decompression time: {} seconds'.format(
         size, end - start))
