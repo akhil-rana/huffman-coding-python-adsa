38e0f49b8ed9ad69f086db0d3e31f2c26c167102
akhil-rana <akhilrana0001@gmail.com>
2022-12-07 19:07:14 +0530

    Remove: unnecessary comments

--- decompress.py
+++ decompress.py
@@ -2,44 +2,40 @@ import sys
 import huffmancoding
 
 
-# Command line main application function.
 def main(args):
-	# Handle command line arguments
-	if len(args) != 2:
-		sys.exit("Usage: python adaptive-huffman-decompress.py InputFile OutputFile")
-	inputfile, outputfile = args
-	
-	# Perform file decompression
-	with open(inputfile, "rb") as inp, open(outputfile, "wb") as out:
-		decompress(huffmancoding.BitInputStream(inp), out)
+    if len(args) != 2:
+        sys.exit("Usage: python adaptive-huffman-decompress.py InputFile OutputFile")
+    inputfile, outputfile = args
+
+    with open(inputfile, "rb") as inp, open(outputfile, "wb") as out:
+        decompress(huffmancoding.BitInputStream(inp), out)
 
 
 def decompress(bitin, out):
-	initfreqs = [1] * 257
-	freqs = huffmancoding.FrequencyTable(initfreqs)
-	dec = huffmancoding.HuffmanDecoder(bitin)
-	dec.codetree = freqs.build_code_tree()  # Use same algorithm as the compressor
-	count = 0  # Number of bytes written to the output file
-	while True:
-		# Decode and write one byte
-		symbol = dec.read()
-		if symbol == 256:  # EOF symbol
-			break
-		out.write(bytes((symbol,)))
-		count += 1
-		
-		# Update the frequency table and possibly the code tree
-		freqs.increment(symbol)
-		if (count < 262144 and is_power_of_2(count)) or count % 262144 == 0:  # Update code tree
-			dec.codetree = freqs.build_code_tree()
-		if count % 262144 == 0:  # Reset frequency table
-			freqs = huffmancoding.FrequencyTable(initfreqs)
+    initfreqs = [1] * 257
+    freqs = huffmancoding.FrequencyTable(initfreqs)
+    dec = huffmancoding.HuffmanDecoder(bitin)
+    dec.codetree = freqs.build_code_tree()
+    count = 0
+    while True:
+        # Decode and write one byte
+        symbol = dec.read()
+        if symbol == 256:  # EOF symbol
+            break
+        out.write(bytes((symbol,)))
+        count += 1
+
+        # Update the frequency table and possibly the code tree
+        freqs.increment(symbol)
+        if (count < 262144 and is_power_of_2(count)) or count % 262144 == 0:  # Update code tree
+            dec.codetree = freqs.build_code_tree()
+        if count % 262144 == 0:  # Reset frequency table
+            freqs = huffmancoding.FrequencyTable(initfreqs)
 
 
 def is_power_of_2(x):
-	return x > 0 and x & (x - 1) == 0
+    return x > 0 and x & (x - 1) == 0
 
 
-# Main launcher
 if __name__ == "__main__":
-	main(sys.argv[1 : ])
+    main(sys.argv[1:])
